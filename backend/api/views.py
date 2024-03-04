de4205d14013830d3df8a4ab274f81150664fc91
yutaro okuda <yutaro.okuda.76@gmail.com>
2024-03-04 19:13:48 +0800

    get tasks

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.permissions import AllowAny
+from rest_framework import generics
+from .models import Task
+from rest_framework import viewsets
+from .serializers import TaskSerializer
 
 # Create your views here.
+class TaskViewSet(viewsets.ModelViewSet):
+  queryset = Task.objects.all()
+  serializer_class = TaskSerializer
